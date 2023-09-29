318e08ef1d5c361a2090ed047df2856ac37d0ea1
ubuntu <ubuntu@gmail.com>
2023-09-29 18:41:36 +0800

    增加字段

--- controller/Update.go
+++ controller/Update.go
@@ -1,1 +1,17 @@
 package controller
+
+import (
+	"common/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ActionUpdate 更新
+type ActionUpdate struct {
+	Model model.IModel
+}
+
+// Update 更新 - put
+func (ths *ActionUpdate) Update(c *gin.Context) {
+
+}
--- model/Base.go
+++ model/Base.go
@@ -7,4 +7,5 @@ type IModel interface {
 	Delete(*gorm.DB, map[string]interface{}) (interface{}, error)
 	Update(*gorm.DB, map[string]interface{}, map[string]interface{}) (interface{}, error)
 	Create(*gorm.DB, map[string]interface{}) (interface{}, error)
+	Fields() []string
 }
