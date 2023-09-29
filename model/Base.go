a51b7d2f2b67517dc6098b077c5c8d1e989d8bff
ubuntu <ubuntu@gmail.com>
2023-09-29 18:29:29 +0800

    增加接口

--- model/Base.go
+++ model/Base.go
@@ -5,4 +5,6 @@ import "gorm.io/gorm"
 type IModel interface {
 	GetAll(*gorm.DB, ...interface{}) (interface{}, int64, error)
 	Delete(*gorm.DB, map[string]interface{}) (interface{}, error)
+	Update(*gorm.DB, map[string]interface{}, map[string]interface{}) (interface{}, error)
+	Create(*gorm.DB, map[string]interface{}) (interface{}, error)
 }
