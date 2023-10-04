df9a3eba54b8ab4e4ebd2916346bc53c2eef81ce
golang <winpc@gmail.com>
2023-10-05 03:49:21 +0800

    增加添加方法

--- controller/Create.go
+++ controller/Create.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"common/clients"
 	"common/model"
+	"common/request"
+	"common/response"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,5 +16,27 @@ type ActionCreate struct {
 
 // Create 创建 - post
 func (ths *ActionCreate) Create(c *gin.Context) {
+	// 过滤字段
+	requestData := c.Request.PostForm    // 获取请求数据
+	tableFields := ths.Model.Fields()    // 获取表字段
+	realData := map[string]interface{}{} // 过滤后的数据
+	for _, field := range tableFields {
+		if val, exists := requestData[field]; exists {
+			realData[field] = val
+		}
+	}
 
+	platform := request.GetPlatform(c)              // 获取平台
+	db, err := clients.GetMySQLByPlatform(platform) // 获取数据库连接
+	if err != nil {
+		response.Err(c, "获取DB失败")
+		return
+	}
+
+	if _, err := ths.Model.Create(db, realData); err != nil {
+		response.Err(c, "创建失败")
+		return
+	}
+
+	response.Ok(c)
 }
--- controller/Update.go
+++ controller/Update.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"common/clients"
 	"common/model"
+	"common/request"
+	"common/response"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,5 +16,36 @@ type ActionUpdate struct {
 
 // Update 更新 - put
 func (ths *ActionUpdate) Update(c *gin.Context) {
+	id := c.GetInt("id") // 获取ID
+	if id == 0 {
+		response.Err(c, "id不能为空")
+		return
+	}
 
+	// 过滤字段
+	requestData := c.Request.PostForm    // 获取请求数据
+	tableFields := ths.Model.Fields()    // 获取表字段
+	realData := map[string]interface{}{} // 过滤后的数据
+	for _, field := range tableFields {
+		if val, exists := requestData[field]; exists {
+			realData[field] = val
+		}
+	}
+
+	platform := request.GetPlatform(c)              // 获取平台
+	db, err := clients.GetMySQLByPlatform(platform) // 获取数据库连接
+	if err != nil {
+		response.Err(c, "获取DB失败")
+		return
+	}
+
+	cond := map[string]interface{}{
+		"id": id,
+	}
+	if _, err := ths.Model.Update(db, realData, cond); err != nil {
+		response.Err(c, "更新失败")
+		return
+	}
+
+	response.Ok(c)
 }
